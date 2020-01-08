6078ca1815438454252ccd13c2c9701575a6fa7e
shannonwells <shannonwells@users.noreply.github.com>
2020-01-08 15:58:51 -0800

    updates after rebase

--- retrievalmarket/network/network.go
+++ retrievalmarket/network/network.go
@@ -3,7 +3,6 @@ package network
 import (
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/libp2p/go-libp2p-core/peer"
-	peer "github.com/libp2p/go-libp2p-peer"
 )
 
 type RetrievalQueryStream interface {
