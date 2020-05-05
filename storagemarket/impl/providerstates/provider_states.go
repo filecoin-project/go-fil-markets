2bddcd8d3ee3ed2de04896fe8c9dd79646b5dcd8
hannahhoward <hannah@hannahhoward.net>
2020-05-04 18:04:34 -0700

    feat(storagemarket): cleanup WaitForMessage
    Modify WaitForMessage signature -- as it turns out, we cannot wait for messages with confidences,
    only state changes (for the most part)

--- storagemarket/impl/providerstates/provider_states.go
+++ storagemarket/impl/providerstates/provider_states.go
@@ -192,11 +192,14 @@ func EnsureProviderFunds(ctx fsm.Context, environment ProviderDealEnvironment, d
 func WaitForFunding(ctx fsm.Context, environment ProviderDealEnvironment, deal storagemarket.MinerDeal) error {
 	node := environment.Node()
 
-	return node.WaitForMessage(deal.AddFundsCid, storagemarket.ChainConfidence, func(code exitcode.ExitCode, bytes []byte) error {
-		if code == exitcode.Ok {
-			return ctx.Trigger(storagemarket.ProviderEventFunded)
+	return node.WaitForMessage(deal.AddFundsCid, func(code exitcode.ExitCode, bytes []byte, err error) error {
+		if err != nil {
+			return ctx.Trigger(storagemarket.ProviderEventNodeErrored, xerrors.Errorf("AddFunds errored: %w", err))
 		}
-		return ctx.Trigger(storagemarket.ProviderEventNodeErrored, xerrors.Errorf("AddFunds exit code: %w", code))
+		if code != exitcode.Ok {
+			return ctx.Trigger(storagemarket.ProviderEventNodeErrored, xerrors.Errorf("AddFunds exit code: %s", code.String()))
+		}
+		return ctx.Trigger(storagemarket.ProviderEventFunded)
 	})
 }
 
@@ -220,32 +223,35 @@ func PublishDeal(ctx fsm.Context, environment ProviderDealEnvironment, deal stor
 
 // WaitForPublish waits for the publish message on chain and sends the deal id back to the client
 func WaitForPublish(ctx fsm.Context, environment ProviderDealEnvironment, deal storagemarket.MinerDeal) error {
-	return environment.Node().WaitForMessage(deal.PublishCid, storagemarket.ChainConfidence, func(code exitcode.ExitCode, retBytes []byte) error {
-		if code == exitcode.Ok {
-			var retval market.PublishStorageDealsReturn
-			err := retval.UnmarshalCBOR(bytes.NewReader(retBytes))
-			if err != nil {
-				return err
-			}
-
-			err = environment.SendSignedResponse(ctx.Context(), &network.Response{
-				State:          storagemarket.StorageDealProposalAccepted,
-				Proposal:       deal.ProposalCid,
-				PublishMessage: &deal.PublishCid,
-			})
-
-			if err != nil {
-				return ctx.Trigger(storagemarket.ProviderEventSendResponseFailed, err)
-			}
-
-			if err := environment.Disconnect(deal.ProposalCid); err != nil {
-				log.Warnf("closing client connection: %+v", err)
-			}
-
-			return ctx.Trigger(storagemarket.ProviderEventDealPublished, retval.IDs[0])
+	return environment.Node().WaitForMessage(deal.PublishCid, func(code exitcode.ExitCode, retBytes []byte, err error) error {
+		if err != nil {
+			return ctx.Trigger(storagemarket.ProviderEventDealPublishError, xerrors.Errorf("PublishStorageDeals errored: %w", err))
+		}
+		if code != exitcode.Ok {
+			return ctx.Trigger(storagemarket.ProviderEventDealPublishError, xerrors.Errorf("PublishStorageDeals exit code: %s", code.String()))
+		}
+		var retval market.PublishStorageDealsReturn
+		err = retval.UnmarshalCBOR(bytes.NewReader(retBytes))
+		if err != nil {
+			return ctx.Trigger(storagemarket.ProviderEventDealPublishError, xerrors.Errorf("PublishStorageDeals error unmarshalling result: %w", err))
 		}
 
-		return ctx.Trigger(storagemarket.ProviderEventDealPublishError, xerrors.Errorf("PublishStorageDeals exit code: %w", code))
+		err = environment.SendSignedResponse(ctx.Context(), &network.Response{
+			State:          storagemarket.StorageDealProposalAccepted,
+			Proposal:       deal.ProposalCid,
+			PublishMessage: &deal.PublishCid,
+		})
+
+		if err != nil {
+			return ctx.Trigger(storagemarket.ProviderEventSendResponseFailed, err)
+		}
+
+		if err := environment.Disconnect(deal.ProposalCid); err != nil {
+			log.Warnf("closing client connection: %+v", err)
+		}
+
+		return ctx.Trigger(storagemarket.ProviderEventDealPublished, retval.IDs[0])
+
 	})
 }
 
