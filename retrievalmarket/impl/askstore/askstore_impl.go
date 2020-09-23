c99bb3cc9a8f3d7482c6a4cd3d99942cc7c00597
Ingar Shu <ingar@users.noreply.github.com>
2020-09-23 09:37:44 -0700

    Persist retrieval ask on disk (#410)
    * Bugfix: Store datastore key in the storage ask repository
    
    * Add an AskStore interface/impl for retrievalmarket
    
    * Use AskStore in provider impl

---
+++ retrievalmarket/impl/askstore/askstore_impl.go
@@ -0,0 +1,116 @@
+package askstore
+
+import (
+	"bytes"
+	"sync"
+
+	"github.com/ipfs/go-datastore"
+	"golang.org/x/xerrors"
+
+	cborutil "github.com/filecoin-project/go-cbor-util"
+
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
+)
+
+// AskStoreImpl implements AskStore, persisting a retrieval Ask
+// to disk. It also maintains a cache of the current Ask in memory
+type AskStoreImpl struct {
+	lk  sync.RWMutex
+	ask *retrievalmarket.Ask
+	ds  datastore.Batching
+	key datastore.Key
+}
+
+// NewAskStore returns a new instance of AskStoreImpl
+// It will initialize a new default ask and store it if one is not set.
+// Otherwise it loads the current Ask from disk
+func NewAskStore(ds datastore.Batching, key datastore.Key) (*AskStoreImpl, error) {
+	s := &AskStoreImpl{
+		ds:  ds,
+		key: key,
+	}
+
+	if err := s.tryLoadAsk(); err != nil {
+		return nil, err
+	}
+
+	if s.ask == nil {
+		// for now set a default retrieval ask
+		defaultAsk := &retrievalmarket.Ask{
+			PricePerByte:            retrievalmarket.DefaultPricePerByte,
+			UnsealPrice:             retrievalmarket.DefaultUnsealPrice,
+			PaymentInterval:         retrievalmarket.DefaultPaymentInterval,
+			PaymentIntervalIncrease: retrievalmarket.DefaultPaymentIntervalIncrease,
+		}
+
+		if err := s.SetAsk(defaultAsk); err != nil {
+			return nil, xerrors.Errorf("failed setting a default retrieval ask: %w", err)
+		}
+	}
+	return s, nil
+}
+
+// SetAsk stores retrieval provider's ask
+func (s *AskStoreImpl) SetAsk(ask *retrievalmarket.Ask) error {
+	s.lk.Lock()
+	defer s.lk.Unlock()
+
+	return s.saveAsk(ask)
+}
+
+// GetAsk returns the current retrieval ask, or nil if one does not exist.
+func (s *AskStoreImpl) GetAsk() *retrievalmarket.Ask {
+	s.lk.RLock()
+	defer s.lk.RUnlock()
+	if s.ask == nil {
+		return nil
+	}
+	ask := *s.ask
+	return &ask
+}
+
+func (s *AskStoreImpl) tryLoadAsk() error {
+	s.lk.Lock()
+	defer s.lk.Unlock()
+
+	err := s.loadAsk()
+
+	if err != nil {
+		if xerrors.Is(err, datastore.ErrNotFound) {
+			// this is expected
+			return nil
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (s *AskStoreImpl) loadAsk() error {
+	askb, err := s.ds.Get(s.key)
+	if err != nil {
+		return xerrors.Errorf("failed to load most recent retrieval ask from disk: %w", err)
+	}
+
+	var ask retrievalmarket.Ask
+	if err := cborutil.ReadCborRPC(bytes.NewReader(askb), &ask); err != nil {
+		return err
+	}
+
+	s.ask = &ask
+	return nil
+}
+
+func (s *AskStoreImpl) saveAsk(a *retrievalmarket.Ask) error {
+	b, err := cborutil.Dump(a)
+	if err != nil {
+		return err
+	}
+
+	if err := s.ds.Put(s.key, b); err != nil {
+		return err
+	}
+
+	s.ask = a
+	return nil
+}
