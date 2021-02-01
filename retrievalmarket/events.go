126d3030a0c354216f37088940dbab8c5f3cc3c9
Dirk McCormick <dirkmdev@gmail.com>
2021-02-01 16:28:04 +0100

    feat: skip payment channel creation if total price is zero

--- retrievalmarket/events.go
+++ retrievalmarket/events.go
@@ -119,6 +119,10 @@ const (
 	// the client that all blocks were sent for the deal, and the client is
 	// waiting for the last blocks to arrive
 	ClientEventWaitForLastBlocks
+
+	// ClientEventPaymentChannelSkip is fired when the total deal price is zero
+	// so there's no need to set up a payment channel
+	ClientEventPaymentChannelSkip
 )
 
 // ClientEvents is a human readable map of client event name -> event description
