e6cf83118d9d49d159446e7b998ae6ce9a05c272
aarshkshah1992 <aarshkshah1992@gmail.com>
2021-07-29 15:19:14 +0530

    log storage client events

--- storagemarket/impl/dtutils/dtutils.go
+++ storagemarket/impl/dtutils/dtutils.go
@@ -86,16 +86,16 @@ func ClientDataTransferSubscriber(deals EventReceiver) datatransfer.Subscriber {
 			return
 		}
 
+		// Note: We ignore data transfer progress events (they do not affect deal state)
+		log.Debugw("processing storage client dt event", "event", datatransfer.Events[event.Code], "proposalCid", voucher.Proposal, "channelID",
+			channelState.ChannelID(), "channelState", datatransfer.Statuses[channelState.Status()])
+
 		if channelState.Status() == datatransfer.Completed {
 			err := deals.Send(voucher.Proposal, storagemarket.ClientEventDataTransferComplete)
 			if err != nil {
 				log.Errorf("processing dt event: %s", err)
 			}
 		}
-
-		// Translate from data transfer events to client FSM events
-		// Note: We ignore data transfer progress events (they do not affect deal state)
-		log.Debugw("processing storage client dt event", "event", datatransfer.Events[event.Code], "proposalCid", voucher.Proposal)
 
 		err := func() error {
 			switch event.Code {
@@ -116,7 +116,8 @@ func ClientDataTransferSubscriber(deals EventReceiver) datatransfer.Subscriber {
 			}
 		}()
 		if err != nil {
-			log.Errorf("processing dt event: %s", err)
+			log.Errorw("error processing storage client dt event", "event", datatransfer.Events[event.Code], "proposalCid", voucher.Proposal, "channelID",
+				channelState.ChannelID(), "err", err)
 		}
 	}
 }
