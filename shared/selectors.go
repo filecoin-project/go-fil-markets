d1f645a26acf3c6e6f48ea6a0d29cabf40c5d8f1
Peter Rabbitson <ribasushi@protocol.ai>
2021-10-08 11:11:05 +0200

    Proprly deprecate as understood by the golang toolchain
    Co-authored-by: Daniel Martí <mvdan@mvdan.cc>

--- shared/selectors.go
+++ shared/selectors.go
@@ -5,6 +5,6 @@ import (
 	selectorparse "github.com/ipld/go-ipld-prime/traversal/selector/parse"
 )
 
-// AllSelector is a compatibility alias for an entire DAG non-matching-selector
-// Use selectorparse.CommonSelector_ExploreAllRecursively directly in new code
+// Deprecated: AllSelector is a compatibility alias for an entire DAG non-matching-selector.
+// Use github.com/ipld/go-ipld-prime/traversal/selector/parse.CommonSelector_ExploreAllRecursively instead.
 func AllSelector() ipld.Node { return selectorparse.CommonSelector_ExploreAllRecursively }
