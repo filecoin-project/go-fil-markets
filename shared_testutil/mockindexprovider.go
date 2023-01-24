916ad0469ddf1f60c4a958e2e98480dbaaa6f66d
Neel Virdy <neelvirdy@gmail.com>
2023-01-24 17:10:28 -0500

    Fix provider imports

--- shared_testutil/mockindexprovider.go
+++ shared_testutil/mockindexprovider.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"sync"
 
+	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-libp2p/core/peer"
+
 	provider "github.com/ipni/index-provider"
 	"github.com/ipni/index-provider/metadata"
-	"github.com/libp2p/go-libp2p/core/peer"
 )
 
 type MockIndexProvider struct {
